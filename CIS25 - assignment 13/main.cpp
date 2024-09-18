f873d956438c412e9ef20e3d0aca22015ba53276
maya-nachiappan <maya.n102304@gmail.com>
2024-09-18 03:19:38 -0700

    initial

--- CIS25 - assignment 13/main.cpp
+++ CIS25 - assignment 13/main.cpp
@@ -1,14 +1,29 @@
 //
-//  main.cpp
 //  CIS25 - assignment 13
 //
 //  Created by maya nachiappan on 9/16/24.
 //
+// program that asks a user to give a list of temperatures then reports the average temperature
 
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main()
+{
+    double avgTemp;
+    double sumTemp=0;
+    int num;
+    
+    cout << "How many temperatures do you have: " << endl;
+    cin >> num;
+    
+    double temp[num];
+    cout << "Enter the temperatures: " << endl;
+    for (int i=0; i<num; i++)
+    {
+        getline(cin, temp[i]);
+        sumTemp=sumTemp+ temp[i];
+    }
+avgTemp= sumTemp
 }
