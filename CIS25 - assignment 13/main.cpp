7b0f37fcd0201300e4b22eaddc66e4829358cd99
maya-nachiappan <maya.n102304@gmail.com>
2024-10-14 01:27:59 -0700

    added comments and gitignore file

--- CIS25 - assignment 13/main.cpp
+++ CIS25 - assignment 13/main.cpp
@@ -11,6 +11,7 @@ using namespace std;
 
 int main()
 {
+    // initializing variables
     int max; // maximum amount of temperatures that can be taken
     double sum=0.0;
     int count=0;
@@ -20,40 +21,43 @@ int main()
     char unit;
     
     
-    // inputting the amount of temperatures
+    // inputting the amount of temperatures the user has
     cout << "enter the amount of temperatures you have: " << endl;
     cin >> max;
+    
+    // input the unit of the temperatures used
     cout << "enter the unit, 'F' or 'C'" << endl;
     cin >> unit;
     
+    // creating array to store number temperatures provided by user
     double temperatures[max];
     
-    for (int i=0; i<max; i++)
+    for (int i=0; i<max; i++) // for loop to enter temperatures in array
     {
-        cout << "enter temperature " << i+1 <<":" << endl;
+        cout << "enter temperature " << i+1 <<":" << endl; // the user is prompted till each array index is filled
         cin >> temp;
         
         
-        temperatures[count] = temp;
-        sum += temp;
-        count++;
+        temperatures[count] = temp; // temperatures stored in array index
+        sum += temp; // temperatures keep adding to sum
+        count++; // count is incremented
         
     }
     
         
   if (count>0)
 {
-    double average=sum/max;
+    double average=sum/max; // average temp is sum divided by max amount of temperatures
     
     // formulas
-    farenheit = (average*9.0/5.0)+32.0;
-    celsius = (average-32.0)*(5.0/9.0);
+    farenheit = (average*9.0/5.0)+32.0; // conversion to farenheit
+    celsius = (average-32.0)*(5.0/9.0); // conversion to celsius
     
-    if ( unit=='F')
+    if ( unit=='F') // if unit is farenheit
     {
         cout << "the average temperature is: " << average << " degrees farenheit and " << celsius << " degrees celsius"<< endl;
     }
-    else if (unit=='C')
+    else if (unit=='C') // if unit is celsius
     {
         cout << "the average temperature is " << average << " degrees celsius and " << farenheit <<
         " degrees farenheit" << endl;
